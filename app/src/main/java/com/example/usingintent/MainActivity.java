4f2026772cc387f8b16df6324f3880aa9cfbeb88
tonyBred <galihrizkisetiadi18@gmail.com>
2021-04-07 15:07:03 +0700

    Returning Results from an Intent

--- app/src/main/java/com/example/usingintent/MainActivity.java
+++ app/src/main/java/com/example/usingintent/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int request_Code = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
-        startActivity(new Intent("com.example.usingintent.SecondActivity"));
+        startActivityForResult(new Intent("com.example.usingintent.SecondActivity"), request_Code);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/usingintent/SecondActivity.java
+++ app/src/main/java/com/example/usingintent/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.example.usingintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void onClick(View view){
+        Intent data = new Intent();
+
+        EditText txt_username = (EditText) findViewById(R.id.txtUsernamee);
+
+        data.setData(Uri.parse(txt_username.getText().toString()));
+        setResult(RESULT_OK, data);
+
+        finish();
     }
 
 }
